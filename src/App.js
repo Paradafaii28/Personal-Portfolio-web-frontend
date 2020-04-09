a696aecd6c350b99786de0e7b2af647f702b14af
github username <Paradafaii28@gmail.com>
2020-04-09 14:23:55 -0600

    add animation and link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import CustomNavBar from './components/CustomNavBar'
 import './App.css';
 import Home from "./components/Home"
 import ContactMe from "./components/ContactMe"
+import Profile from "./components/Profile"
+import MyAccount from "./components/MyAccount"
+import LogIn from "./components/LogIn"
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <div className="App">
         <CustomNavBar/>
       <Route exact path='/' component={Home} />  
+      <Route exact path='/profile' component={Profile} />  
+      <Route exact path='/myaccount' component={MyAccount} />  
+      <Route exact path='/login' component={LogIn} />  
       <Route exact path='/contactMe' component={ContactMe} />  
       </div>
       </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CustomNavBar from './CustomNavBar'
 import me from "../photo/parada.png"
 
 function App() {
@@ -18,37 +17,6 @@ function App() {
             </div>
             <div className="container-photo">
                 <img src={me} className="image"/>
-            </div>
-        </div>
-        <div className="container-profile">
-            <div className="container-about">
-                <h1>About Me</h1>
-            </div>
-            <div className="container-p">
-                <p>My name is Parada (Alice) Richardson. I am originally from Thailand, and I have lived in the USA for almost 4 years now.<br/>
-                    My move to America has been a dream come true. I earned a degree with a major in interior design, and a minor in architecture.
-                    <br/>
-                    I love the opportunity to be creative and work to solve problems. Becoming a software developer is the next chapter in my life,
-                    and I very interested and passionate to be able to learn this new skill.<br/>
-                    <br/>
-                    Creative and enthusiastic. Very personable, with a talent for developing sold client relationships.
-                    Outstanding work ethic with a dedication to exceeding expectation. Willingness to learn and eager to always improve upon my skills.
-                </p>
-            </div>
-        </div>
-        <div className="container-skill">
-            <div className="container-topic">
-                <h1>What Can I do</h1>
-            </div>
-            <div className="container-listOfSkill">
-                <ul className="container-list">
-                    <li>Ruby on Rails</li>
-                    <li>Javascript</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>SQL</li>
-                    <li>React</li>
-                </ul>
             </div>
         </div>
     </div>
